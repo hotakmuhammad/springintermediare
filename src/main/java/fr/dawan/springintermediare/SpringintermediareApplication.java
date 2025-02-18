ad6e70271f624adfd9ee92e79c79f98bc9261b66
Muhammad Shahid <me911khan@gmail.com>
2025-02-18 08:58:24 +0100

    initial commits

---
+++ src/main/java/fr/dawan/springintermediare/SpringintermediareApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.springintermediare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintermediareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringintermediareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/springintermediare/SpringintermediareApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.springintermediare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringintermediareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
